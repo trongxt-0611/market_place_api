ef3e1b0743664de2567efe2f66ef892bc259b715
pandoraolaole <trongga9a@gmail.com>
2022-12-26 22:55:25 +0700

    setup bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   validates :email, uniqueness: true
   validates_format_of :email, with: Settings.email_regex
   validates :password_digest, presence: true
+  has_secure_password 
 end
