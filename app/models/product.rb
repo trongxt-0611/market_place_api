7f00229ec6cb1f0f453016fcad0fccb02b043b4d
pandoraolaole <trongga9a@gmail.com>
2023-01-07 21:39:12 +0700

    Adds search scope on the product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,15 @@ class Product < ApplicationRecord
   belongs_to :user
   validates :price, numericality: { greater_than_or_equal_to: Settings.product.min_price }, presence: true
   validates :title, :user_id,  presence: true
+
+  scope :filter_by_title, lambda {|keyword|
+    where("LOWER(title) LIKE?", "%#{keyword.downcase}%")
+  }
+  scope :above_or_equal_to_price, -> (price) {
+    where("price >=?", price)
+  }
+  scope :below_or_equal_to_price, -> (price) {
+    where("price <=?", price)
+  }
+  scope :recent, -> { order(:updated_at) }
 end
