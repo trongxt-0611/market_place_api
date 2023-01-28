f732e0c09de3702aae633c56f657ec8acc633e0b
pandoraolaole <trongga9a@gmail.com>
2023-01-28 22:22:26 +0700

    adds caching for the serializers

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -2,4 +2,5 @@ class OrderSerializer
   include FastJsonapi::ObjectSerializer
   has_many :products
   belongs_to :user
+  cache_options enabled: true, cache_length: 12.hours
 end
--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -2,4 +2,5 @@ class ProductSerializer
   include FastJsonapi::ObjectSerializer
   attributes :title, :published, :price, :updated_at
   belongs_to :user
+  cache_options enabled: true, cache_length: 12.hours
 end
